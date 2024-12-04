dcf163a76b8381b1b7d7ffbae1efe5fb34f9aa92
Jan Hovland <jan.hovland@lyse.net>
2024-12-04 08:26:02 +0100

    Initial Commit

---
+++ SaveData/SaveDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveDataApp.swift
+//  SaveData
+//
+//  Created by Jan Hovland on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SaveDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
